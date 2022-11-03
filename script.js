b92e67c2d2e81893e8bcb81474c0516eaa6f6c2d
Daniel Aoulou <aouloudan@icloud.com>
2022-11-03 16:18:36 -0400

    added winState

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const body = document.querySelector('body');
 const newGameButton = document.querySelector('#newGame');
+const cells = [];
 
 const playerSwitches = document.createElement('div');
 playerSwitches.id = 'playerSwitches';
@@ -12,7 +13,8 @@ p2Switch.id = 'p2';
 p2Switch.textContent = ('Player 2');
 playerSwitches.appendChild(p1Switch);
 playerSwitches.appendChild(p2Switch);
-const cells = [];
+
+active = false;
 
 const Player = (token) => {
     let thistoken = token;
@@ -23,21 +25,25 @@ const Player = (token) => {
     }
     const checkWin = () => {
         let win = false;
-        if ((cellsHeld.includes('0') && cellsHeld.includes('1') && cellsHeld.includes('2'))
-        || (cellsHeld.includes('3') && cellsHeld.includes('4') && cellsHeld.includes('5'))
-        || (cellsHeld.includes('6') && cellsHeld.includes('7') && cellsHeld.includes('8'))
-        || (cellsHeld.includes('0') && cellsHeld.includes('3') && cellsHeld.includes('6'))
-        || (cellsHeld.includes('1') && cellsHeld.includes('4') && cellsHeld.includes('7'))
-        || (cellsHeld.includes('2') && cellsHeld.includes('5') && cellsHeld.includes('8'))
-        || (cellsHeld.includes('0') && cellsHeld.includes('4') && cellsHeld.includes('8'))
-        || (cellsHeld.includes('2') && cellsHeld.includes('4') && cellsHeld.includes('8'))) {
+        if ((cellsHeld.includes('0') && cellsHeld.includes('1') && cellsHeld.includes('2')) ||
+            (cellsHeld.includes('3') && cellsHeld.includes('4') && cellsHeld.includes('5')) ||
+            (cellsHeld.includes('6') && cellsHeld.includes('7') && cellsHeld.includes('8')) ||
+            (cellsHeld.includes('0') && cellsHeld.includes('3') && cellsHeld.includes('6')) ||
+            (cellsHeld.includes('1') && cellsHeld.includes('4') && cellsHeld.includes('7')) ||
+            (cellsHeld.includes('2') && cellsHeld.includes('5') && cellsHeld.includes('8')) ||
+            (cellsHeld.includes('0') && cellsHeld.includes('4') && cellsHeld.includes('8')) ||
+            (cellsHeld.includes('2') && cellsHeld.includes('4') && cellsHeld.includes('6'))) {
             win = true;
         }
         if (win) {
-            alert('WIN');
-        }
+            winGame(thistoken);
+            return true;
+        } else return false;
     }
-    return {captureCell, checkWin, cellsHeld}
+    return {
+        captureCell,
+        checkWin
+    }
 }
 
 var player1 = true; //false if player 2 is playing
@@ -45,7 +51,7 @@ var player1 = true; //false if player 2 is playing
 const gameboard = (() => {
     let gameboard = document.createElement('div');
     gameboard.id = 'gameboard';
-    for(let i = 0; i < 9; i++) {
+    for (let i = 0; i < 9; i++) {
         let boardCell = document.createElement('div');
         boardCell.classList.add('cell')
         boardCell.id = `cell${i}`;
@@ -60,44 +66,73 @@ const gameboard = (() => {
         body.appendChild(gameboard);
         body.appendChild(playerSwitches);
     };
-    return {createBoard};
+    return {
+        createBoard,
+    };
 })();
 
 newGameButton.addEventListener('click', () => {
+    if (active) {
+        location.reload();
+    }
     gameboard.createBoard();
     p1 = Player('X');
     p2 = Player('O')
     playGame(p1, p2);
+    active = true;
 });
 
 function playGame(p1, p2) {
     cells.forEach(cell => {
         cell.addEventListener('click', () => {
-            if(player1) {
+            if (player1) {
                 cell.classList.add('p1cell');
                 p1.captureCell(cell);
                 player1 = !player1;
-                for(let cell of cells) {
-                    if(!cell.classList.contains('p1cell')) {
+                for (let cell of cells) {
+                    if (!cell.classList.contains('p1cell')) {
                         cell.textContent = 'O';
                     }
                 }
-                p1.checkWin();
                 p1Switch.classList.remove('currentPlayer');
                 p2Switch.classList.add('currentPlayer');
+                if (p1.checkWin()) {
+                    for (let cell of cells) {
+                        if (cell.classList.contains('p1cell')) cell.classList.add('winner');
+                        p2Switch.classList.remove('currentPlayer');
+                        p1Switch.classList.add('winner');
+                    }
+                }
             } else {
                 cell.classList.add('p2cell');
                 p2.captureCell(cell);
                 player1 = !player1;
-                for(let cell of cells) {
-                    if(!cell.classList.contains('p2cell')) {
+                for (let cell of cells) {
+                    if (!cell.classList.contains('p2cell')) {
                         cell.textContent = 'X';
                     }
                 }
-                p2.checkWin();
                 p2Switch.classList.remove('currentPlayer');
                 p1Switch.classList.add('currentPlayer');
+                if (p2.checkWin()) {
+                    for (let cell of cells) {
+                        if (cell.classList.contains('p2cell')) cell.classList.add('winner');
+                        p1Switch.classList.remove('currentPlayer');
+                        p2Switch.classList.add('winner');
+                    }
+                }
             }
         })
     })
+}
+
+function winGame(player) {
+    for (let cell of cells) {
+        cell.style.cssText += 'pointer-events: none;'
+    }
+    if (player == 'X') {
+        alert('Player 1 has won!');
+    } else {
+        alert('Player 2 has won!');
+    }
 }
